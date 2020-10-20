f4a1b4ab501fb8f47d77a68f1462a058f620db20
Bimesh De Silva <Bimde@users.noreply.github.com>
2020-10-19 22:43:07 -0400

    Update index.js (#1)

--- fufillment/index.js
+++ fufillment/index.js
@@ -12,7 +12,7 @@ app.handle('stackr_push', (conv) => {
     conv.user.params.stackr = [];
   }
   if (!conv.intent.params.item) {
-  	conv.user.params.stackr_push = "nothing! Say stackr push followed by something to push onto your stack. For example, Stackr push water bottle will add water bottle to your stack!";
+  	conv.user.params.stackr_push = "nothing! Say Idea Stack push followed by something to push onto your stack. For example, Stack Bot push pencil crayons will add pencil crayons to your stack!";
   } else {
     conv.user.params.stackr.push(conv.intent.params.item.resolved);
     conv.user.params.stackr_push = conv.user.params.stackr[conv.user.params.stackr.length - 1];
@@ -26,7 +26,7 @@ app.handle('stackr_peek', (conv) => {
   // conv.add('Hello world from fulfillment');
   if (!conv.user.params.stackr || conv.user.params.stackr.length == 0) {
     conv.user.params.stackr = [];
-  	conv.user.params.stackr_peek = "Nothing's in your stack, try saying stacker push to add some stuff!";
+  	conv.user.params.stackr_peek = "Nothing's in your stack, try saying Idea Stack push to add some stuff!";
   } else {
   	conv.user.params.stackr_peek = conv.user.params.stackr[conv.user.params.stackr.length - 1];
   }
@@ -39,7 +39,7 @@ app.handle('stackr_pop', (conv) => {
   // conv.add('Hello world from fulfillment');
   if (!conv.user.params.stackr || conv.user.params.stackr.length == 0) {
     conv.user.params.stackr = [];
-  	conv.user.params.stackr_pop = "nothing! Try saying stacker push to add some stuff!";
+  	conv.user.params.stackr_pop = "nothing! Try saying Idea Stack push to add some stuff!";
   } else {
   	conv.user.params.stackr_pop = conv.user.params.stackr.pop();
   }
